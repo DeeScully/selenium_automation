6370b3969b37bb5227c30eb0e5554e61a3152018
Wai Chu <wchu-consultant@scholastic.com>
2018-11-21 14:32:03 -0500

    created first pg obj test

--- vrap_smoke/pageobjects/tflx/loginscreen.py
+++ vrap_smoke/pageobjects/tflx/loginscreen.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from values import strings
+from webdriver import Driver
 import allure
 import string
 import random
@@ -11,6 +12,7 @@ class TflxLogInScrn:
 
     def __init__(self, driver):
         self.driver = driver
+        self.driver.navigate(strings.tflx_p_url)
         self.user_name_field = WebDriverWait(self.driver.instance, 10).until(
             EC.visibility_of_element_located((By.XPATH, ".//input[@id='username']")))
         self.password_field = WebDriverWait(self.driver.instance, 10).until(
@@ -19,3 +21,11 @@ class TflxLogInScrn:
             EC.visibility_of_element_located((By.XPATH, ".//button[@id='loginButton']")))
 
 
+    def test_tflx_login(self):
+        self.user_name_field.send_keys(strings.shared_un)
+        self.password_field.send_keys(strings.shared_pw)
+        self.log_in_btn.click()
+
+        tflx_header = WebDriverWait(self.driver.instance, 10).until(
+            EC.visibility_of_element_located((By.XPATH, ".//div[@id='mainheadercontainer']")))
+        assert tflx_header.is_displayed()
--- vrap_smoke/testcases/smoke_tests.py
+++ vrap_smoke/testcases/smoke_tests.py
@@ -1,5 +1,6 @@
 import unittest
 import allure
+import pageobjects.tflx.loginscreen
 from webdriver import Driver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -14,23 +15,29 @@ class TestAllApps(unittest.TestCase):
         # self.driver.navigate(strings.base_url)
 
     @allure.step('tflx log in with proper creds')
-    def test_tflx(self):
-        self.driver.navigate(strings.tflx_p_url)
-        user_name_field = WebDriverWait(self.driver.instance, 10).until(
-            EC.visibility_of_element_located((By.XPATH, ".//input[@id='username']")))
-        password_field = WebDriverWait(self.driver.instance, 10).until(
-            EC.visibility_of_element_located((By.XPATH, ".//input[@id='password']")))
-        log_in_btn = WebDriverWait(self.driver.instance, 10).until(
-            EC.visibility_of_element_located((By.XPATH, ".//button[@id='loginButton']")))
+    def test_tflx_login(self):
+        screen = pageobjects.tflx.loginscreen.TflxLogInScrn(self.driver)
+        screen.test_tflx_login()
+        # self.driver.navigate(strings.tflx_p_url)
+        # user_name_field = WebDriverWait(self.driver.instance, 10).until(
+        #     EC.visibility_of_element_located((By.XPATH, ".//input[@id='username']")))
+        # password_field = WebDriverWait(self.driver.instance, 10).until(
+        #     EC.visibility_of_element_located((By.XPATH, ".//input[@id='password']")))
+        # log_in_btn = WebDriverWait(self.driver.instance, 10).until(
+        #     EC.visibility_of_element_located((By.XPATH, ".//button[@id='loginButton']")))
+        #
+        # user_name_field.send_keys(strings.shared_un)
+        # password_field.send_keys(strings.shared_pw)
+        # log_in_btn.click()
+        #
+        # tflx_header = WebDriverWait(self.driver.instance, 10).until(
+        #     EC.visibility_of_element_located((By.XPATH, ".//div[@id='mainheadercontainer']")))
+        # assert tflx_header.is_displayed()
 
-        user_name_field.send_keys(strings.shared_un)
-        password_field.send_keys(strings.shared_pw)
-        log_in_btn.click()
 
-        tflx_header = WebDriverWait(self.driver.instance, 10).until(
-            EC.visibility_of_element_located((By.XPATH, ".//div[@id='mainheadercontainer']")))
-        assert tflx_header.is_displayed()
-
+    # @allure.step('tflx randomly select a category')
+    # def test_tflx_select_cat(self):
+    #
 
 
 
